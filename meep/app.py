079f727bc57123d4ed5ddcd49b265c737b49b10c
Niti Chaudhary <nitic04@gmail.com>
2024-09-01 22:10:43 -0400

    Added change to fix run on boot error

--- meep/app.py
+++ meep/app.py
@@ -151,4 +151,4 @@ def robot_loop():
 if __name__ == '__main__':
     import threading
     threading.Thread(target=robot_loop).start()
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
